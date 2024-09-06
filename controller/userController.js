ad87aa18e732f82540b7bddc33573d839708249b
aman <amannema34@gmail.com>
2024-09-06 15:31:34 +0530

    update message

--- controller/userController.js
+++ controller/userController.js
@@ -144,7 +144,7 @@ const login = async (req, res) => {
 
 const test = async (req, res) => {
 	try {
-		return res.send("Hello Aman docker");
+		return res.send("Hello Amanss docker");
 		// return res.status(200).send({
 		// 	users: req.user,
 		// 	message: `Welcome ${req.user.userName}`,
